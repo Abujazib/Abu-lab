9452b2e2d77f056f82f8fd164edb445c2fd88627
Abujazib <abujazib555@gmail.com>
2025-09-30 11:27:01 -0400

    mobilesidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import "..//Css/Navbar.css";
+import CancelRoundedIcon from "@mui/icons-material/CancelRounded";
 import ClassRoundedIcon from "@mui/icons-material/ClassRounded";
 import Button from "@mui/material/Button";
 import { FaBars } from "react-icons/fa6";
@@ -11,6 +12,15 @@ import IconButton from "@mui/material/IconButton";
 import Drawer from "@mui/material/Drawer";
 
 function Navbar() {
+  
+  const [open,setOpen]= useState(false);
+  const handleopen = () =>{
+    setOpen(true);
+  };
+  const handleclose = () => {
+    setOpen(false);
+  };
+
   return (
     <>
       <Box sx={{ flexGrow: 1 }} className="navbox">
@@ -55,45 +65,48 @@ function Navbar() {
               </div>
               <div className="more">
                 <IconButton className="more-button">
-                  <FaBars />
+                  <FaBars onClick={handleopen}/>
                 </IconButton>
               </div>
             </Toolbar>
           </div>
         </AppBar>
       </Box>
-      <div className="side-bar">
-        <Drawer> 
-          <div className="side-links">
-            <Button>
-              <a href="">Home</a>
-            </Button>
-            <Button>
-              <a href="">About Us</a>
-            </Button>
-            <Button>
-              <a href="">Our Test</a>
-            </Button>
-            <Button>
-              <a href="">Contact Us</a>
-            </Button>
-          </div>
-          <div className="side-appionment-link">
-            <Button
-              className="glow-btn"
-              variant="outlined"
-              onClick={() =>
-                window.open(
-                  "https://abulab-79efc.web.app/appointments",
-                  "_blank"
-                )
-              }
-            >
-              <ClassRoundedIcon /> Book an Appoinment
-            </Button>
-          </div>
-        </Drawer>
-      </div>
+      <Drawer
+        variant="persistent"
+        anchor="left"
+        open={open}
+        className="side-bar"
+      >
+        <IconButton className="close-button" onClick={handleclose}>
+          <CancelRoundedIcon />
+        </IconButton>
+        <div className="links">
+          <Button onClick={handleclose}>
+            <a href="">Home</a>
+          </Button>
+          <Button onClick={handleclose}>
+            <a href="">About Us</a>
+          </Button>
+          <Button onClick={handleclose}>
+            <a href="">Our Test</a>
+          </Button>
+          <Button onClick={handleclose}>
+            <a href="">Contact Us</a>
+          </Button>
+        </div>
+        <div className="appionment-link">
+          <Button
+            className="glow-btn"
+            variant="outlined"
+            onClick={() =>
+              window.open("https://abulab-79efc.web.app/appointments", "_blank")
+            }
+          >
+            <ClassRoundedIcon /> Book an Appoinment
+          </Button>
+        </div>
+      </Drawer>
     </>
   );
 }
