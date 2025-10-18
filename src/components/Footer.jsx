197641c26ad3d0123bc69c781e21dbe40a0a8328
Abujazib <abujazib555@gmail.com>
2025-10-18 18:11:03 -0400

    iteam

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "..//Css/Footer.css";
 import CopyrightIcon from "@mui/icons-material/Copyright";
-import Abulogo from "..//assets/abulablogo.jpg";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import EmailRoundedIcon from "@mui/icons-material/EmailRounded";
@@ -11,19 +10,25 @@ import IconButton from "@mui/material/IconButton";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import Tooltip from "@mui/material/Tooltip";
 import InstagramIcon from "@mui/icons-material/Instagram";
+import { cardData, socialMedia } from "../uidata/Contactdata";
 
 function Footer() {
+  const contactItems = cardData.filter(
+    (item) => item.id === 2 || item.id === 3
+  );
+
   return (
     <>
       <Card
         sx={{
-          color: "#ffebaf",
-          backgroundColor: "#4c9db0",
+          color: "#f6d55c",
+          backgroundColor: "#006989",
           fontFamily: "Open Sans",
           padding: "10px",
           display: "flex",
           justifyContent: "space-between",
         }}
+        className="card-con"
       >
         <CardContent>
           <h2>Abu Laboratory</h2>
@@ -37,39 +42,45 @@ function Footer() {
             <Link to="/contact-us">Contact Us</Link>
           </div>
         </CardContent>
-        <CardContent>
+        <CardContent sx={{ textAlign: "center" }}>
           <h2>Follow as on</h2>
-          <Tooltip title="Instagram">
-            <IconButton
-              sx={{ color: "white", "&:hover": { color: "#ffebaf" } }}
-            >
-              <InstagramIcon />
-            </IconButton>
-          </Tooltip>
-          <Tooltip title="FaceBook">
-            <IconButton
-              sx={{ color: "white", "&:hover": { color: "#ffebaf" } }}
-            >
-              <FacebookIcon />
-            </IconButton>
-          </Tooltip>
+          {contactItems.map((iteam) => (
+            <Tooltip key={iteam.id} title={iteam.heading}>
+              <IconButton
+                sx={{ color: "white", "&:hover": { color: "#f6d55c" } }}
+                onClick={() => window.open(iteam.url, "_blank")}
+              >
+                {iteam.icon}
+              </IconButton>
+            </Tooltip>
+          ))}
+        </CardContent>
+        <CardContent sx={{ textAlign: "center" }}>
+          <h2>Contact as</h2>
           <Tooltip title="Gmail">
             <IconButton
-              sx={{ color: "white", "&:hover": { color: "#ffebaf" } }}
+              sx={{ color: "white", "&:hover": { color: "#f6d55c" } }}
             >
               <EmailRoundedIcon />
             </IconButton>
           </Tooltip>
           <Tooltip title="Call">
             <IconButton
-              sx={{ color: "white", "&:hover": { color: "#ffebaf" } }}
+              sx={{ color: "white", "&:hover": { color: "#f6d55c" } }}
             >
               <CallRoundedIcon />
             </IconButton>
           </Tooltip>
         </CardContent>
       </Card>
-      <Card sx={{backgroundColor: "#ffebaf", textAlign:"center", color:"#4c9db0", height:"100px"}}>
+      <Card
+        sx={{
+          backgroundColor: "#f6d55c",
+          textAlign: "center",
+          color: "#006989",
+          height: "100px",
+        }}
+      >
         <CardContent>
           <CopyrightIcon />
         </CardContent>
